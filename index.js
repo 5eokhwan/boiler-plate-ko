b2e29065ac870b95d75c61d6c92b1a3bc69a2b81
5eokhwan <exist909@naver.com>
2021-01-01 12:35:08 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://sh:m80803@boilerplate.xmc4z.mongodb.net/boilerplate?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-//
+app.get("/", (req, res) => res.send("Hello Worldd!~!~!~!~"));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.post("/register", (req, res) => {
+  //회원가입할때 필요한 정보들을 client에서 가져오면 그것들을 db에 넣어줌
+  const user = new User(req.body); //bodyParser가 있어서 가능한것
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example .... ${port}`));
